241102d24cdcdd9617c2f11ba25fc214ba4135d6
boziyoung <50167927+boziyoung@users.noreply.github.com>
2022-08-08 11:44:25 +0800

    Create sendmethod.py

---
+++ common/sendmethod.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+
+class SendMethod:
+    def __init__(self, method, url):
+        self.method = method
+        self.url = url
+
+    def send_method(self, **kwargs):
+        if self.method == 'get' or 'delete':
+            response = requests.request(method=self.method, url=self.url, **kwargs)
+        elif self.method == 'post' or 'put':
+            response = requests.request(method=self.method, url=self.url, **kwargs)
+        else:
+            print('请求方式错误')
+            response = None
+
+        # 对请求的结果进行操作
+        if self.method == 'delete':
+            return response.status_code
+        else:
+            print(f'状态码：{response.status_code}')
+            # time.sleep(2)
+            # bs = BeautifulSoup(response.text, 'html.parser')
+            return response
